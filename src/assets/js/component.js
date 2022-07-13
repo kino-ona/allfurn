f90d1efc17d5888172c2b8faca9e818378917d26
ona-hyejinkim <hyejin.kim@ona.co.kr>
2022-07-13 11:21:16 +0900

    컴포넌트 작업중3

---
+++ src/assets/js/component.js
@@ -0,0 +1,45 @@
+
+$(window).on('load', function () {
+
+  function openModal(name) {
+    $(`${name}`).css('display', 'block');
+    $('body').css('overflow','hidden')
+  };
+  
+  function closeModal (name) {
+    $(`${name}`).css('display', 'none');
+  }
+
+  $('.modal-type--single').on('click', function() {
+    openModal('#modal-type--single');
+   });
+
+   $('.modal-type--pair').on('click', function() {
+    openModal('#modal-type--pair');
+   });
+
+   $('.modal-type--basic').on('click', function() {
+    openModal('#modal-type--basic');
+   });
+
+   $('.modal-close').on('click', function (event) {
+    event.preventDefault();
+    closeModal('#modal-type--basic');
+   })
+
+  $('.comment__like').on('click', function (event) {
+    event.preventDefault();
+    console.log();
+
+   if(!$(this)[0].classList.contains('like--active')) {
+    $(this).addClass('like--active');
+    $(this).find("path").attr("fill", "#FB4760");
+   }else {
+    $(this).removeClass('like--active');
+    $(this).find("path").attr("fill", "#BDBDBD");
+   }
+    
+  })
+
+
+});
