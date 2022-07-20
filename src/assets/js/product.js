466a494addc381f3a7f98eebd6cf4b70ef0f3d56
jiyonghwa <onlystrong@naver.com>
2022-07-20 10:08:55 +0900

    선택 목록 임시 수정

--- src/assets/js/product.js
+++ src/assets/js/product.js
@@ -2,10 +2,35 @@ $(".product-bookmark").on("click", function () {
   $(this).toggleClass("active");
 });
 
-$(".dropdown").on("click", function () {
+$(".dropdown.dropdown--01").on("click", function () {
   $(this).toggleClass("dropdown--active");
 });
+$(".dropdown__item.dropdown__item--01").on("click", function (evnet) {
+  evnet.preventDefault();
+  $(".dropdown__title.dropdown__title--01").text(
+    $(this).text().trim(" ").split("\n")[0]
+  );
+});
 
+$(".dropdown.dropdown--02").on("click", function () {
+  $(this).toggleClass("dropdown--active");
+});
+$(".dropdown__item.dropdown__item--02").on("click", function (evnet) {
+  evnet.preventDefault();
+  $(".dropdown__title.dropdown__title--02").text(
+    $(this).text().trim(" ").split("\n")[0]
+  );
+});
+
+$(".dropdown.dropdown--03").on("click", function () {
+  $(this).toggleClass("dropdown--active");
+});
+$(".dropdown__item.dropdown__item--03").on("click", function (evnet) {
+  evnet.preventDefault();
+  $(".dropdown__title.dropdown__title--03").text(
+    $(this).text().trim(" ").split("\n")[0]
+  );
+});
 $(".order-info__title").on("click", function () {
   $(this).toggleClass("active");
 });
