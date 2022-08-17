a33dafeeccaecc423a3897e9b5873b636eaea3b2
kino <66652533+kino-ona@users.noreply.github.com>
2022-08-17 16:28:53 +0900

    headernav js  참고수정

--- src/assets/js/headernav.js
+++ src/assets/js/headernav.js
@@ -16,6 +16,11 @@ const headerNav = () => {
 		globalsearch.addClass(headerNavActive);
 		navModal.addClass(navModalActive);
 
+		if(globalsearch.find('.globalsearch__banner .swiper-container').length > 0) {
+			searchBanner();
+		}
+		gnbReset()
+
 		const blank = document.querySelector('.globalsearch-modal.globalsearch-modal--active');
 		if(blank === null) return; else {blank.addEventListener('click', () => { headerNavClose(); })};
 	});
@@ -29,68 +34,69 @@ const headerNavClose = () => {
 	navModal.removeClass(navModalActive);
 }
 
-if(globalsearch.find('.globalsearch__banner .swiper-container').length > 0) {
-	var swiper = new Swiper('#headerNavBanner', {
-		loop: true,
-		slidesPerView: 1,
-		spaceBetween: 0,
-		allowTouchMove: false,
-		paginationClickable: false,
-		keyboard: false,
-		navigation: {
-			nextEl: '.carousel-nav-next',
-			prevEl: '.carousel-nav-prev',
-		},
-    pagination: {
-      el: '#headerNavBanner .swiper-pagination',
-      type: 'fraction',
-			formatFractionCurrent: function (number) {
-				let fraction = '0';
-				if (number < 10) {
-					return fraction + number;
-				} else {
-					return number;
-				}
+function searchBanner() {
+		var swiper = new Swiper('#headerNavBanner', {
+			loop: true,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			allowTouchMove: false,
+			paginationClickable: false,
+			keyboard: false,
+			navigation: {
+				nextEl: '.carousel-nav-next',
+				prevEl: '.carousel-nav-prev',
 			},
-			formatFractionTotal: function (number) {
-				let fraction = '0';
-				if (number < 10) {
-					return fraction + number;
-				} else {
-					return number;
-				}
+			pagination: {
+				el: '#headerNavBanner .swiper-pagination',
+				type: 'fraction',
+				formatFractionCurrent: function (number) {
+					let fraction = '0';
+					if (number < 10) {
+						return fraction + number;
+					} else {
+						return number;
+					}
+				},
+				formatFractionTotal: function (number) {
+					let fraction = '0';
+					if (number < 10) {
+						return fraction + number;
+					} else {
+						return number;
+					}
+				},
 			},
-    },
-		autoplay: {
-			delay: 3000,
-		},
-		observer: true,
-		observeParents: true,
-	});
+			autoplay: {
+				delay: 3000,
+			},
+			observer: true,
+			observeParents: true,
+		});
 }
 
 headerNav();
 
-// var swiper = new Swiper('#eventkvSwipeHeader', {
-// 	autoplay: {
-// 		delay: 3000,
-// 	},
-// 	loop: true,
-// 	slidesPerView: 1,
-// 	spaceBetween: 0,
-// 	paginationClickable: true,
-// 	keyboard: true,
-// 	speed: 400,
-// 	pagination: {
-// 		el: '#eventkvSwipeHeader .swiper-pagination',
-// 		type: 'fraction',
-// 	},
-// 	navigation: {
-// 		nextEl: '.swiper-button-next',
-// 		prevEl: '.swiper-button-prev',
-// 	},
-// });
-
+function gnbCategoryBanner() {
+	var gnbSwiper = new Swiper('#eventkvSwipeHeader', {
+		autoplay: {
+			delay: 3000,
+		},
+		loop: true,
+		slidesPerView: 1,
+		spaceBetween: 0,
+		paginationClickable: true,
+		keyboard: true,
+		speed: 400,
+		pagination: {
+			el: '#eventkvSwipeHeader .swiper-pagination',
+			type: 'fraction',
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+}
 // 카테고리 gnb
 $(".gnb__menu li a").on("click", function () {
 	if($(this).hasClass("active") === false) {
@@ -114,6 +120,10 @@ $(".gnb__category .category-wrap a").on("click", function () {
 		$(".category-wrap--active").css("display", "block");
 		$(".category-banner-wrap").css("display", "block");
 	}
+
+	if($('.category-banner .swiper-slide').length > 0) {
+		gnbCategoryBanner();
+	}
 });
 $(".gnb__category .category-wrap--active a").on("click", function () {
 	if($(this).hasClass("active") === false) {
@@ -121,3 +131,11 @@ $(".gnb__category .category-wrap--active a").on("click", function () {
 		$(".gnb__category .category-wrap--active a").not($(this)).removeClass("active");
 	}
 });
+
+function gnbReset() {
+	$(".gnb__category").css("display", "none");
+	$(".gnb__menu li a").removeClass('active');
+	$('.head__gnb .gnb__category li a').removeClass('active');
+	$('.category-wrap--active').css("display", "none");
+	$('.category-banner-wrap').css("display", "none");
+}
